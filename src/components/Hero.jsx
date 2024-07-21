351340baaeda95433dc82527fe13f9d78ff5e696
MuzzammilN <naqvimuzzammil@gmail.com>
2024-07-20 21:52:36 -0500

    Finished the gym app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,18 @@
 import React from 'react'
+import Button from './Button'
 
-function Hero() {
-  return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center
-    max-w-[900px] w-full mx-auto'>
-      <div className='flex flex-col gap-4'>
-      <p>IT'S TIME TO GET</p>
-      <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Swole<span className='text-blue-400'>normous</span></h1>
-      </div>
-      <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become 
-        <span className='text-blue-400 font-medium'> unbelievably swolenormous</span> and 
-        accept all risks of becoming the local 
-        <span className='text-blue-400 font-medium'> mass montrosity</span>, afflicted 
-        with severe body dismorphia, unable to 
-        fit through doors.</p>
+export default function Hero() {
+    return (
+        <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
+            <div className='flex flex-col gap-4'>
 
-        <button className=' rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow sm:px-8 py-2 sm:py-4 relative duration-200'>
-      <p>Accept & Begin</p>
-        </button>
-
-
-    </div>
-  )
+                <p>IT'S TIME TO GET</p>
+                <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>E<span className='text-red-400'>normous</span></h1>
+            </div>
+            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that I may become <span className='text-red-400 font-medium'>unbelievably enormous</span> and accept all risks of becoming the local <span className='text-red-400 font-medium'>mass montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
+            <Button func={() => {
+                window.location.href = '#generate'
+            }} text={"Accept & Begin"}></Button>
+        </div>
+    )
 }
-
-export default Hero
